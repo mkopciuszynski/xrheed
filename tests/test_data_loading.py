46f33811aaa29c27f80566b81061d1074737069e
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2025-06-03 11:20:13 +0200

    🧹 Tiny refractoring

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,6 +1,6 @@
 import unittest
 from pathlib import Path
-import xrheed
+
 from xrheed.io import load_data
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,19 +10,19 @@ class TestDataLoading(unittest.TestCase):
     def setUp(self):
         # Load the data using the plugin
         test_data_path = Path(__file__).parent / "data" / "si_111_7x7_112.raw"
-        self.r = load_data(str(test_data_path), plugin="umcs_arpes_raw")
+        self.rheed_image = load_data(str(test_data_path), plugin="umcs_arpes_raw")
 
     def test_set_center(self):
         # Test the set_center method
         try:
-            self.r.R.set_center()
+            self.rheed_image.R.set_center()
         except Exception as e:
             self.fail(f"set_center method raised an exception: {e}")
 
     def test_plot_image(self):
         # Test the plot_image method
         try:
-            self.r.R.plot_image()
+            self.rheed_image.R.plot_image()
         except Exception as e:
             self.fail(f"plot_image method raised an exception: {e}")
 
