bd221b299f734b96f92f7a9d5b09d9f097c630f6
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2025-07-04 16:25:56 +0200

    ✨🔨 Refactor find_horizontal_center and find_vertical_center functions to use more sophisticated method to find the center of an image.
    Still in progress..

--- src/xrheed/preparation/alignment.py
+++ src/xrheed/preparation/alignment.py
@@ -1,28 +1,64 @@
 import xarray as xr
 import numpy as np
 
-from scipy.signal import savgol_filter
-import ruptures as rpt
+import lmfit as lf
+
+
+from xrheed.preparation.filters import gaussian_filter_profile
 
 
 def find_horizontal_center(image: xr.DataArray) -> float:
 
     profile = image.sum("y")
-    return float(image.x[profile.argmax()])
+    profile_smoothed = gaussian_filter_profile(profile, sigma=1.0)
+    max_pos = profile_smoothed.x.values[np.argmax(profile_smoothed.values)]
+
+    # TODO improve this adding additional horizontal_center search
+
+    return max_pos
 
 
 def find_vertical_center(image: xr.DataArray, shadow_edge_width: float = 5.0) -> float:
     # Shadow edges defines the vertical center 0, 0 point of an image
 
-    profile = image.sel(x=slice(-20, 20)).mean("x")
-    edge_width_px = int(shadow_edge_width * image.R.screen_scale)
+    x_range = 20.0
+    x_mirror_spot_size = 3.0
 
-    smoothed_data = savgol_filter(profile, window_length=edge_width_px, polyorder=1)
+    profile = image.where(
+        ((image.x >= -x_range) & (image.x <= -x_mirror_spot_size))
+        | ((image.x >= x_mirror_spot_size) & (image.x <= x_range)),
+        drop=True,
+    ).sum(dim="x")
 
-    gradient = np.diff(smoothed_data)
+    sigma = shadow_edge_width * 0.1
+    profile_smoothed = gaussian_filter_profile(profile, sigma=sigma)
 
-    algo = rpt.Dynp(model="l2").fit(gradient)
-    breakpoints = algo.predict(n_bkps=2)
+    max_idx = profile_smoothed.argmax()
+    subprofile = profile_smoothed.isel(y=slice(max_idx.item(), None))
 
-    edge_pos = image.y[breakpoints[0]]
-    return float(edge_pos)
+    # Prepare data for fitting
+    x = subprofile["y"].values
+    y = subprofile
+
+    y -= y.min()
+    y /= y.max()
+
+    sigmoid_model = lf.Model(_linear_plus_sigmoid)
+
+    params = sigmoid_model.make_params(a=0.0, b=0.0, L=1.0, k=0.1, x0=0.0)
+
+    result = sigmoid_model.fit(y, params=params, x=x)
+    sigmoid_center = result.params["x0"].value
+    sigmoid_k = result.params["k"].value
+
+    return sigmoid_center - sigmoid_k * 2.0
+
+
+# Define sigmoid function for fitting
+def _sigmoid(x, amp, k, x0, back):
+    return amp / (1 + np.exp(-k * (x - x0))) + back
+
+
+# Model: Linear + Sigmoid
+def _linear_plus_sigmoid(x, a, b, L, k, x0):
+    return a * x + b + L / (1 + np.exp(-k * (x - x0)))
