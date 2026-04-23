64ad7a8a10e1659ce71b41233b665fc74a4d4368
Marek Kopciuszynski <mkopciuszynski@gmail.com>
2026-04-23 15:47:45 +0200

    ✨ Expose `transform_stack_to_kxky` in conversion module

--- src/xrheed/conversion/__init__.py
+++ src/xrheed/conversion/__init__.py
@@ -4,6 +4,11 @@ e.g., transforming between pixel space and kx-ky space.
 """
 
 from .base import convert_gx_gy_to_sx_sy, convert_sx_to_ky
-from .image import transform_image_to_kxky
+from .image import transform_image_to_kxky, transform_stack_to_kxky
 
-__all__ = ["convert_gx_gy_to_sx_sy", "convert_sx_to_ky", "transform_image_to_kxky"]
+__all__ = [
+    "convert_gx_gy_to_sx_sy",
+    "convert_sx_to_ky",
+    "transform_image_to_kxky",
+    "transform_stack_to_kxky",
+]
